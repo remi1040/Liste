43266932fd2da57df66ede8790aa5b84306c0ce7
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-24 11:26:49 +0200

    Add files via upload

---
+++ Liste/Liste/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<char> list = new List<char>();
+
+        list.Add('a');
+        list.Add('b');
+        list.Add('c');
+
+        foreach (char c in list)
+        {
+            Console.WriteLine(c);
+        }
+        
+
+        list.RemoveAt(0);
+        list.Remove('c');
+
+        list[0] = 'd';
+
+        foreach (char c in list)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
